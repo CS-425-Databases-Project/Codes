d3713eb488ddfaaa8fb829d7007c5ab40f003e04
RilesScott <32188086+RilesScott@users.noreply.github.com>
2018-07-26 19:41:23 -0500

    Update Login.java

--- Java Files/cs425_databases/src/cs425_databases/Login.java
+++ Java Files/cs425_databases/src/cs425_databases/Login.java
@@ -6,13 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JPasswordField;
 
 public class Login {
 
 	private JFrame frmHttpwwwstorecom;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField textField_2;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -42,45 +44,53 @@ public class Login {
 	 */
 	private void initialize() {
 		frmHttpwwwstorecom = new JFrame();
-		frmHttpwwwstorecom.setTitle("http://www.store.com/login.aspx");
-		frmHttpwwwstorecom.setBounds(100, 100, 450, 300);
+		frmHttpwwwstorecom.getContentPane().setBackground(Color.BLACK);
+		frmHttpwwwstorecom.setTitle("http://www.store.com/login.aspx?src=common");
+		frmHttpwwwstorecom.setBounds(100, 100, 350, 250);
 		frmHttpwwwstorecom.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHttpwwwstorecom.getContentPane().setLayout(null);
 		
 		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(98, 77, 74, 16);
+		lblUsername.setForeground(Color.WHITE);
+		lblUsername.setBounds(65, 37, 74, 16);
 		frmHttpwwwstorecom.getContentPane().add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(98, 105, 74, 16);
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setBounds(65, 65, 74, 16);
 		frmHttpwwwstorecom.getContentPane().add(lblPassword);
 		
 		JLabel lblZipcode = new JLabel("Zipcode:");
-		lblZipcode.setBounds(98, 133, 74, 16);
+		lblZipcode.setForeground(Color.WHITE);
+		lblZipcode.setBounds(65, 93, 74, 16);
 		frmHttpwwwstorecom.getContentPane().add(lblZipcode);
 		
 		textField = new JTextField();
-		textField.setBounds(182, 72, 130, 26);
+		textField.setBounds(150, 32, 130, 26);
 		frmHttpwwwstorecom.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(182, 100, 130, 26);
-		frmHttpwwwstorecom.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		textField_2 = new JTextField();
-		textField_2.setBounds(182, 128, 130, 26);
+		textField_2.setBounds(150, 88, 130, 26);
 		frmHttpwwwstorecom.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(217, 166, 95, 29);
+		btnLogin.setBounds(189, 126, 95, 29);
 		frmHttpwwwstorecom.getContentPane().add(btnLogin);
 		
 		JButton btnSignUp = new JButton("Sign Up");
-		btnSignUp.setBounds(88, 166, 117, 29);
+		btnSignUp.setBounds(58, 126, 117, 29);
 		frmHttpwwwstorecom.getContentPane().add(btnSignUp);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(150, 60, 130, 26);
+		frmHttpwwwstorecom.getContentPane().add(passwordField);
+		 
+		JLabel error = new JLabel("* invalid username or password");
+		error.setForeground(Color.RED);
+		error.setBounds(70, 167, 212, 16);
+		frmHttpwwwstorecom.getContentPane().add(error);
+		error.setVisible(false);
 	}
-
 }
