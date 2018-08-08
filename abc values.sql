1540c8885eda4c41432f9957ec9409bd7b744c05
spizana1 <31364095+spizana1@users.noreply.github.com>
2018-08-08 15:26:06 -0500

    Add files via upload

---
+++ abc values.sql
@@ -0,0 +1,17 @@
+insert into customer values ('23456', 'Joe', 'Elston');
+
+insert into staff values ('11111', 'Mary', 'Jones', 'Peoria', 'IN', 'Halsted', '60625', 'sales');
+
+insert into login values ('22221', 'john1', 'iit1'),
+('12222', 'mary2', 'm222');
+
+insert into cinfo values ('23456','0123456789ABCDEF','01','2018', 'state', 'chicago', 'il', '60616'); 
+
+
+insert into warehouse values 
+('IL201','Damen', 'Chicago','IL',' 60622', 23000.89),
+('PO111','Washington','Peoria','IL',' 61611', 443459.56), 
+('NP548','159th','Orland Park','IL',' 60462', 658880.89),
+('MR786','79th','Meriville', 'IN','46410', 14787.35);
+
+
