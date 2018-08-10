ff2562696a4a9ceb1db13a3cc973dfa68be04afa
daaaaaaanielxu <41762708+daaaaaaanielxu@users.noreply.github.com>
2018-08-10 14:11:11 -0500

    Update abc values.sql

--- abc values.sql
+++ abc values.sql
@@ -1,6 +1,6 @@
 insert into customer values ('23456', 'Joe', 'Elston');
 
-insert into staff values ('11111', 'Mary', 'Jones', 'Peoria', 'IN', 'Halsted', '60625', 'sales');
+insert into staff values ('11111', 'Mary', 'Jones', 'Peoria', 'IN', 'Halsted', '60625',' ', 'sales');
 
 insert into login values ('22221', 'john1', 'iit1'),
 ('12222', 'mary2', 'm222');
@@ -9,9 +9,9 @@ insert into cinfo values ('23456','0123456789ABCDEF','01','2018', 'state', 'chic
 
 
 insert into warehouse values 
-('IL201','Damen', 'Chicago','IL',' 60622', 23000.89),
-('PO111','Washington','Peoria','IL',' 61611', 443459.56), 
-('NP548','159th','Orland Park','IL',' 60462', 658880.89),
+('IL201','Damen', 'Chicago','IL','60622', 23000.89),
+('PO111','Washington','Peoria','IL','61611', 443459.56), 
+('NP548','159th','Orland Park','IL','60462', 658880.89),
 ('MR786','79th','Meriville', 'IN','46410', 14787.35);
 
 
